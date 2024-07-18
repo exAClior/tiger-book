46b2e14a9d690deed3e37a6641567214a757c721
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-07-18 20:01:42 +0800

    temp up

--- test/test.hs
+++ test/test.hs
@@ -5,3 +5,5 @@ import TestCommonWords (removeSymbolsTest, dummyTest )
 main = do
   putStrLn "Get my handy tests started!"
   defaultMain (testGroup "Common Words Tests" [dummyTest, removeSymbolsTest])
+
+  -- https://github.com/haskell/aeson/blob/master/tests/Tests.hs
